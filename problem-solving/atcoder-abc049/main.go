215800917d8281d2ed311bd39758502cc0b959ac
Aftab Hossain Shakib <73667838+ahshakib@users.noreply.github.com>
2025-05-08 20:36:03 +0600

    add atcoder abc049 problem

---
+++ problem-solving/atcoder-abc049/main.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main() {
+  var c string
+  fmt.Scan(&c)
+  
+  if c == "a" || c == "e" || c == "i" || c == "o" || c == "u" {
+    fmt.Println("vowel")
+  } else {
+    fmt.Println("consonant")
+  }
+}
